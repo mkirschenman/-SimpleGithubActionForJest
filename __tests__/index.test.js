c84059bd968ce602209527628d3fe0345b74d6c7
Kirschenman <mkirschenman@qci.com>
2026-03-31 13:42:39 -0500

    added another test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,4 +4,7 @@ describe('an example test', () => {
     it('first example test', () => {
         expect(simpleMathFunctionToTest(1, 2)).toBe(3);
     });
+     it('first example test 2', () => {
+        expect(simpleMathFunctionToTest(2, 2)).toBe(4);
+    });
 });
